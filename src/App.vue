9e9972d25c13e17d31a3819ad55b804ce8e661a4
xiaofeng <309760053@qq.com>
2019-06-07 14:02:07 +0800

    '本次提交的说明'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-switch active-text="按月付费" inactive-text="按年付费"></el-switch>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  mounted() {
+    var a = this.axios({
+      url: "https://api.github.com/users",
+      method: "GET",
+      params: {
+        data: 123
+      }
+    }).then(res => {});
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import Vue from 'vue';
+import ElementUI from "element-ui";
+import axios from 'axios';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import Login from './pages/login.vue';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+const routes=[
+  {path:'/login',component:Login}
+];
+const router=new VueRouter({
+  routes,
+});
+Vue.prototype.axios=axios;
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
